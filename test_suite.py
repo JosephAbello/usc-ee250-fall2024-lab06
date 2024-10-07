1c7917cf55db2c4262ada4739252f0223828810f
JosephAbello <abelloqu@usc.edu>
2024-10-07 14:26:14 -0700

    edited test

--- test_suite.py
+++ test_suite.py
@@ -4,7 +4,7 @@ import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 #using physical pin 11 to blink an LED
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 chan_list = [11]
 GPIO.setup(chan_list, GPIO.OUT)
 
@@ -14,6 +14,9 @@ SPI_DEVICE = 0
 LED_PIN = 30
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
+# Hardware SPI Setup
+GPIO.setup(LED_PIN, GPIO.OUT)
+
 # by taking readings and printing them out, find
 # appropriate threshold levels and set them 
 # accordingly. Then, use them to determine
@@ -21,18 +24,15 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 lux_treshold=0  # change this value
 sound_treshold=0 # change this value
 
-for i in range(5):
-  GPIO.output(LED_PIN, GPIO.HIGH)
-  time.sleep(0.5)
-  GPIO.output(LED_PIN, GPIO.LOW)
-
-
 while True: 
-  time.sleep(0.5) 
-
   #Following commands control the state of the output
   #GPIO.output(pin, GPIO.HIGH)
   #GPIO.output(pin, GPIO.LOW)
+  for i in range(5):
+  GPIO.output(LED_PIN, GPIO.HIGH)
+  time.sleep(0.5)
+  GPIO.output(LED_PIN, GPIO.LOW)
+  time.sleep(0.5)
 
   # get reading from adc 
   # mcp.read_adc(adc_channel)
