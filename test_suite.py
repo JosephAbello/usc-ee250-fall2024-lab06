6caeb50535f15c9ac892f16cfb8602bcbc9d3849
JosephAbello <abelloqu@usc.edu>
2024-10-07 14:40:41 -0700

    debug

--- test_suite.py
+++ test_suite.py
@@ -21,15 +21,21 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 lux_treshold=0  # change this value
 sound_treshold=0 # change this value
 
-while True: 
-  #Following commands control the state of the output
-  #GPIO.output(pin, GPIO.HIGH)
-  #GPIO.output(pin, GPIO.LOW)
-  for i in range(5):
-    GPIO.output(chan_list, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(chan_list, GPIO.LOW)
-    time.sleep(0.5)
+for i in range(5):
+  GPIO.output(chan_list, GPIO.HIGH)
+  time.sleep(0.5)
+  GPIO.output(chan_list, GPIO.LOW)
+  time.sleep(0.5)
 
-  # get reading from adc 
-  # mcp.read_adc(adc_channel)
+# while True: 
+#   #Following commands control the state of the output
+#   #GPIO.output(pin, GPIO.HIGH)
+#   #GPIO.output(pin, GPIO.LOW)
+#   for i in range(5):
+#     GPIO.output(chan_list, GPIO.HIGH)
+#     time.sleep(0.5)
+#     GPIO.output(chan_list, GPIO.LOW)
+#     time.sleep(0.5)
+
+#   # get reading from adc 
+#   # mcp.read_adc(adc_channel)
